3b8d9ee7ac6aeede34b87e666290e37f130bcb1e
Unknown <rodericks@live.nl>
2018-09-28 15:17:54 +0200

    Barebones HP/Damage system

---
+++ Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BaseCharacter : MonoBehaviour
+    {
+        [SerializeField] private float _hitPoints = 10;
+        /// <summary>
+        /// The hitpoints of the character.
+        /// Readonly, if you want to adjust the HP use TakeDamage(float) or Heal(float)
+        /// </summary>
+        public float HitPoints => _hitPoints;
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) Heal(1f);
+            if (Input.GetKeyDown(KeyCode.DownArrow)) TakeDamage(1f);
+        }
+
+        /// <summary>
+        /// Lets the character take damage and decide if it needs to die
+        /// </summary>
+        /// <param name="damage">the amount of damage taken</param>
+        public void TakeDamage(float damage)
+        {
+            AdjustHitpoints(Mathf.Abs(damage) * -1);
+        }
+
+        /// <summary>
+        /// Lets the character heal itself
+        /// </summary>
+        /// <param name="health">the amount of health healed</param>
+        public void Heal(float health)
+        {
+            AdjustHitpoints(Mathf.Abs(health));
+        }
+    
+        private void AdjustHitpoints(float damage)
+        {
+            Debug.Log($"Adjusting hitpoints by {damage} points");
+            _hitPoints += damage;
+            Debug.Log($"Hitpoints {_hitPoints} left");
+            if (_hitPoints <= 0.0f) Die();
+        }
+
+        /// <summary>
+        /// Destroy the object and start the dying behaviour
+        /// </summary>
+        public virtual void Die()
+        {
+            Debug.Log("Dying!");
+            Destroy(this);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,52 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Assets.Scripts.Player
 {
-    [SerializeField] private float _maxSpeed = 2f;
-    [SerializeField] private float _acceleration = 0.3f;
-    [SerializeField] private float _friction = 0.2f;
-    [SerializeField] private Rigidbody2D _rigidbody;
-
-    private Vector2 _velocity;
-
-    // Use this for initialization
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        _maxSpeed += _friction;
-    }
+        [SerializeField] private float _maxSpeed = 2f;
+        [SerializeField] private float _acceleration = 0.3f;
+        [SerializeField] private float _friction = 0.2f;
+        [SerializeField] private Rigidbody2D _rigidbody;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private Vector2 _velocity;
 
-    }
-
-    void FixedUpdate()
-    {
-        _velocity.x = VelocityForAxis("Horizontal", _velocity.x);
-        _velocity.y = VelocityForAxis("Vertical", _velocity.y);
-
-        _rigidbody.velocity = _velocity;
-        Debug.Log($"Current _velocity: {_rigidbody.velocity}");
-    }
-
-    private float VelocityForAxis(string axis, float currentVelocity)
-    {
-        var movement = Input.GetAxisRaw(axis) * _acceleration;
-
-        currentVelocity = Mathf.Clamp(currentVelocity + movement, -_maxSpeed, _maxSpeed);
-
-        if (currentVelocity > 0.0f)
+        // Use this for initialization
+        void Start()
         {
-            currentVelocity -= _friction;
-        }
-        else if (currentVelocity < 0.0f)
-        {
-            currentVelocity += _friction;
+            _maxSpeed += _friction;
         }
 
-        return currentVelocity;
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+            _velocity.x = VelocityForAxis("Horizontal", _velocity.x);
+            _velocity.y = VelocityForAxis("Vertical", _velocity.y);
+
+            _rigidbody.velocity = _velocity;
+        }
+
+        private float VelocityForAxis(string axis, float currentVelocity)
+        {
+            var movement = Input.GetAxisRaw(axis) * _acceleration;
+
+            currentVelocity = Mathf.Clamp(currentVelocity + movement, -_maxSpeed, _maxSpeed);
+
+            if (currentVelocity > 0.0f)
+            {
+                currentVelocity -= _friction;
+            }
+            else if (currentVelocity < 0.0f)
+            {
+                currentVelocity += _friction;
+            }
+
+            return currentVelocity;
+        }
     }
 }
