5a0852fb5f7d8c4a4f8e085f9493cdd5ce418883
Unknown <rodericks@live.nl>
2018-09-29 17:33:19 +0200

    Can shoot random red bally arrows now

---
+++ Assets/Scripts/Generic/ArrowBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Generic
+{
+    public class ArrowBehavior : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            var velocity = transform.right * _speed;
+            _rigidbody.velocity = velocity;
+        }
+    }
+}
--- Assets/Scripts/Player/AttackBehavior.cs
+++ Assets/Scripts/Player/AttackBehavior.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class AttackBehavior : MonoBehaviour {
+namespace Player
+{
+    public class AttackBehavior : MonoBehaviour
+    {
+        [SerializeField] private GameObject _arrowPrefab;
+        [SerializeField] private int _ammo = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) DoAttack();
+        }
+
+        private void DoAttack()
+        {
+            if (_ammo > 0)
+            {
+                Debug.DrawRay(transform.position, transform.right, Color.green, 1);
+                Debug.Log("I shot");
+                _arrowPrefab.transform.rotation = transform.rotation;
+                _arrowPrefab.transform.position = transform.position;
+                Instantiate(_arrowPrefab);
+
+                _ammo--;
+            }
+        }
+    }
 }
