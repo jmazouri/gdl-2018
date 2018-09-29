d196e4e6f5b8849a0c57ac1db2f3f23c0fbf4c84
John Mazouri <jmazouri@gmail.com>
2018-09-29 01:06:04 -0400

    More tile updates

---
+++ Assets/Scripts/Player/CameraFollower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField]
+    private float smoothness;
+
+    private Transform followTarget;
+
+    public void Follow(Transform target)
+    {
+        followTarget = target;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        followTarget = FindObjectOfType<PlayerMovement>().transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var targetPosition = new Vector3(followTarget.position.x, followTarget.position.y, -10);
+
+        transform.position = 
+            Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothness);
+	}
+}
