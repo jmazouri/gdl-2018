3c8230f278fd00fe42915cd031ec70c067a9d99f
Unknown <rodericks@live.nl>
2018-09-29 21:10:41 +0200

    Added arrow damage and despawn

--- Assets/Scripts/Generic/ArrowBehavior.cs
+++ Assets/Scripts/Generic/ArrowBehavior.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using AI;
+using UnityEngine;
 
 namespace Generic
 {
@@ -6,6 +7,7 @@ namespace Generic
     {
         [SerializeField] private float _speed;
         [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private BoxCollider2D _collider;
 
         // Use this for initialization
         void Start()
@@ -18,6 +20,19 @@ namespace Generic
         {
             var velocity = transform.right * _speed;
             _rigidbody.velocity = velocity;
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.IsTouchingLayers(LayerMask.GetMask("Obstacle", "Enemy"))) return;
+
+            var wolf = other.GetComponent<BaseAIController>();
+            if (wolf != null)
+            {
+                wolf.TakeDamage(1);
+            }
+
+            Destroy(gameObject);
         }
     }
 }
