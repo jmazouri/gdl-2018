86fb0bd0b4a87833ea6bc1901170cfdebfe2a9b4
Unknown <rodericks@live.nl>
2018-09-29 13:13:16 +0200

    We are assassins creed now

---
+++ Assets/Scripts/Level/InteractableHay.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Level
+{
+    public class InteractableHay : InteractionTarget
+    {
+        private bool _containsPlayer;
+        private GameObject _player;
+
+        public override void Interact(Interactor interactor)
+        {
+            _player = interactor.gameObject;
+
+            _player.SetActive(false);
+
+            _containsPlayer = true;
+        }
+
+        public override void InternalStart()
+        {
+            
+        }
+
+        public override void InternalUpdate()
+        {
+            if (!_containsPlayer) return;
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _player.SetActive(true);
+            }
+        }
+    }
+}
