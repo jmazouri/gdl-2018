37c8da7108977308cf3c49db17fc21d1642032d7
John Mazouri <jmazouri@gmail.com>
2018-09-29 04:13:38 -0400

    Added signpost interactable & prefabs
    Also added textmeshpro stuff

---
+++ Assets/Scripts/Level/InteractableSign.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableSign : InteractionTarget
+{
+    private bool isShown = false;
+
+    [SerializeField]
+    private GameObject textPopup;
+
+    [SerializeField]
+    [Multiline]
+    private string message;
+
+    public override void Interact(Interactor interactor)
+    {
+        isShown = !isShown;
+    }
+
+    public override void InternalStart()
+    {
+        textPopup.transform.localScale = Vector3.zero;
+        textPopup.GetComponentInChildren<TextMeshPro>().SetText(message);
+    }
+
+    public override void InternalUpdate()
+    {
+        Vector3 targetScale = isShown ? new Vector3(1, 1, 1) : Vector3.zero;
+
+        textPopup.transform.localScale =
+            Vector3.Lerp(textPopup.transform.localScale, targetScale, Time.deltaTime * 8f);
+    }
+}
